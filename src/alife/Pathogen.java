cb1f796a2d90dc2425126cd4eaa6bcff806bd47d
XRose <xyanrose@terpmail.umd.edu>
2014-05-04 05:32:17 -0400

    Update Pathogen.java

--- src/alife/Pathogen.java
+++ src/alife/Pathogen.java
@@ -1,12 +1,34 @@
 package alife;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Pathogen {
-	private short geneticCode;
-	public Pathogen(short gene){
+	private Integer geneticCode;
+	private static Random codeGen = new Random();
+	
+	public Pathogen(int gene){
 		this.geneticCode = gene;
 	}
-	public short getGeneticCode(){
+	public Pathogen(){
+		geneticCode = codeGen.nextInt();
+	}
+	
+	public double getFitness(ArrayList<Pathogen> list){
+		int count = 0;
+		for(Pathogen disease: list){
+			if (this.geneticCode.equals(disease.geneticCode)){
+				count++;
+			}
+		}
+		return count/list.size();
+	}
+
+	
+	
+	public Integer getGeneticCode(){
 		return this.geneticCode;
 	}
+	
 	
 }
